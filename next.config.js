da234daf1a6658bcb0f2721c6bcb71bc94889820
Bence Kovács <kbence55@gmail.com>
2026-02-04 14:41:26 +0200

    Return to default bundling instead of webpack

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@mobiscroll/connect-sdk'],
-  // Using webpack temporarily to resolve symlinks and add SDK source directory, TODO: use default bundler when SDK is published
-  webpack: (config, { isServer }) => {
-    config.resolve.symlinks = true;
-    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../mobiscroll-connect-node/dist')];
-    return config;
-  },
 };
 
 module.exports = nextConfig;
