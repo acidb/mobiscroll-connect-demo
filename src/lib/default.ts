216881d8d456c6809599842d01a383af17952060
Bence Kovács <kbence55@gmail.com>
2026-02-15 19:56:04 +0200

    Fix defaults

--- src/lib/default.ts
+++ src/lib/default.ts
@@ -1,7 +1,11 @@
 const isDev = process.env.NODE_ENV !== 'production';
 
 export const defaultConfig = {
-  mobiscrollClientId: isDev ? process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_ID_DEV : process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_ID,
-  mobiscrollClientSecret: isDev ? process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_SECRET_DEV : process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_SECRET,
+  mobiscrollClientId: isDev
+    ? process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_ID_DEV || process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_ID
+    : process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_ID,
+  mobiscrollClientSecret: isDev
+    ? process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_SECRET_DEV || process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_SECRET
+    : process.env.NEXT_PUBLIC_MOBISCROLL_CLIENT_SECRET,
   mobiscrollRedirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI,
 };
